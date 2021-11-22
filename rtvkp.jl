18157c5933a526ed53b68da4ed885a4c4cf42cfb
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-11-22 14:44:46 -0500

    can run single optimization

--- rtvkp.jl
+++
@@ -1,9 +0,0 @@
-using JuMP
-using GLPK
-using DataFrames
-
-cost_effective = DataFrame(Vaccine_Type=[:A, :B, :C, :D], 
-                           Cost=[2., 5., 3., 4.],
-                           Effectiveness=[50.,70.,60.,80])
-
-
